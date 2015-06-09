3b929dfc7465fc8c8db24c65758074c38c1e026c
Tim Williams <timothy.williams@cern.ch>
2015-06-09 11:06:48 +0100

    added data option to job submission script

--- kstaretappipig/JobSubmit.py
+++ kstaretappipig/JobSubmit.py
@@ -4,14 +4,18 @@ import getpass
 from distutils.util import strtobool
 
 polarity='MagDown'
+datatype='Data'
 
 b=Job()
 b.application=DaVinci(version="v36r5")
-b.application.optsfile='NTupleMaker_{0}.py'.format(polarity)
+if datatype=='MC':
+    b.application.optsfile='NTupleMaker_{0}.py'.format(polarity)
+if datatype=='Data':
+    b.application.optsfile='DNTupleMaker.py'
 b.outputfiles=[DiracFile('Output.root')]
-b.inputdata = b.application.readInputData('MC_12_{0}_Kstar_etap_pipig.py'.format(polarity))
-b.comment='MC_12_{0}_kstar_etap_pipig'.format(polarity)
-b.splitter = SplitByFiles(filesPerJob=2)
+b.inputdata = b.application.readInputData('{0}_12_{1}_Kstar_etap_pipig.py'.format(datatype,polarity))
+b.comment='{0}_12_{1}_kstar_etap_pipig'.format(datatype,polarity)
+b.splitter = SplitByFiles(filesPerJob=5)
 #b.OutputSandbox=["stderr","stdout"]
 
 b.backend=Dirac()
