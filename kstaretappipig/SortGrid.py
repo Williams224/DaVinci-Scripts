3492e915c4e1f1ad921893a28cd5c79ecabc0e20
Tim Williams <timothy.williams@cern.ch>
2015-06-12 16:54:19 +0100

    added kstar rho data.py files

--- kstaretappipig/SortGrid.py
+++ kstaretappipig/SortGrid.py
@@ -1,16 +1,22 @@
 import datetime as dt
 
-finishtime=dt.datetime(2015,6,11,05,00)
+#finishtime=dt.datetime(2015,6,11,05,00)
 
-while dt.datetime.now()<finishtime:
-    MopUpFails(161)
-    MopUpFails(162)
-    MopUpFails(163)
-    GetOutput(161)
-    GetOutput(162)
-    GetOutput(163)
-    MopUpFails(165)
-    MopUpFails(167)
-    GetOutput(165)
-    GetOutput(167)
-              
+#while dt.datetime.now()<finishtime:
+ #   MopUpFails(161)
+  #  MopUpFails(162)
+   # MopUpFails(163)
+    #GetOutput(161)
+    #GetOutput(162)
+   # GetOutput(163)
+   # MopUpFails(165)
+   # MopUpFails(167)
+   # GetOutput(165)
+   # GetOutput(167)
+
+GetOutput(161)
+GetOutput(162)
+GetOutput(163)
+GetOutput(165)
+GetOutput(167)
+
